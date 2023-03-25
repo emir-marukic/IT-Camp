d372388ada73d6588b250e18072fd7a51c48964a
emir-marukic <marukicemir33@hotmail.com>
2023-03-25 11:35:20 +0100

    objecti

--- Java-Script/1.cas/app.js
+++ Java-Script/1.cas/app.js
@@ -70,3 +70,19 @@ object = {
 
 console.log(object);
 console.log(typeof object);
+
+// 3. Set - Struktura podataka za skladistenje vise vrednosti koja ne dozvoljava vise vrednosti koja  ne dozvoljava duplikate. Slicna je nizu (Array), a sintaksta za pravljenje seta je:
+
+set = new Set(["Asija", "Melida", "Dino", 19, 31, 25, "Dino"]);
+// Svako novo pojavljivanje elementa u setu se nece primeniti
+console.log(set);
+
+// 4. Map - Struktura podataka za skladistenje vise vrednosti i slicna je objektu. Sintaksa za pravljenje mape je
+
+map = new Map([
+  ["banane", 160],
+  ["jagode", 260],
+  ["kruske", 140],
+  ["ananas", 289],
+]);
+console.log(map);
