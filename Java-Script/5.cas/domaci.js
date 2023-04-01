60ce5bed5b989c1d5ffa439f36d76b9ff738992f
emir-marukic <marukicemir33@hotmail.com>
2023-04-02 01:17:20 +0200

    domaci if / switch

---
+++ Java-Script/5.cas/domaci.js
@@ -0,0 +1,35 @@
+//1.
+//Napisite program koji na osnovu 2 uneta broja od strane korisnika ispisuje:
+//Povrsinu kvadrata ako su neti isti brojevi
+//Povrsinu pravougaonika ako su neti brojevi razliciti.
+//-----------------------------------------------------
+// const num1 = +prompt("Unesite 2 broja");
+// const num2 = +prompt("Unesie 2 broja");
+
+// if (isNaN(num1) || isNaN(num2)) {
+//   console.log("Niste uneli broj");
+// } else if (num1 <= 0 || num2 <= 0) {
+//   console.log("Nemoguce je uneti brojeve manje od 0");
+// } else if (num1 === num2) {
+//   console.log("Povrsina kvadrata je " + num1 * num2);
+// } else {
+//   console.log("Povrsina pravouganonika je " + num1 * num2);
+// }
+//-----------------------------------------------------
+
+// 2. Zadatak - Switch naredba
+// Korisnik unosi dva realna broja x i y. Napisati progaram koji izracunava i stampa
+// Kolicnik x/y, ako je broj y razlicit od nule
+// a inace ispisuje poruku: Deljenje je nemoguce
+
+//-----------------------------------------------------
+const x = parseInt(+prompt("Unesite 2 broja"));
+const y = parseInt(+prompt("Unesite 2 broja"));
+
+switch (y !== 0) {
+  case true:
+    console.log("Kolicik je: " + x / y);
+    break;
+  default:
+    console.log("Deljenje nije moguce");
+}
--- app2.js
+++
@@ -1,1 +0,0 @@
-// Java Script Switch naredba
