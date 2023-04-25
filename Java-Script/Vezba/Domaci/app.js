7c50fdab7862cf56bbaebdd114c787660db84bdf
emir-marukic <marukicemir33@hotmail.com>
2023-04-25 16:58:06 +0200

    domaci

---
+++ Java-Script/Vezba/Domaci/app.js
@@ -0,0 +1,37 @@
+// Domaci:
+// 1.
+// Ispisati sve brojeve od 1 do 10 koji su parni
+
+// 2.
+// Korisnik unosi 2 broja:
+// Na osnovu toga koji je broj manji, iteracija se vrsi od njega do veceg broja.
+// Ispisuju se brojevi od manjeg ka vecem.
+
+// 1.
+// let i = 1;
+// while (i < 11) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+//-------------------------------
+
+// 2.
+
+// let n = +prompt("Unesite korektan broj ");
+// let k = +prompt("Unesite korektan broj ");
+
+// if (isNaN(n) || isNaN(k)) {
+//   console.log("Niste uneli tacan broj");
+// } else if (n <= k) {
+//   while (n <= k) {
+//     console.log(n);
+//     n++;
+//   }
+// } else {
+//   while (k <= n) {
+//     console.log(k);
+//     k++;
+//   }
+// }
