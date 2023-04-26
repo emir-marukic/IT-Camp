91ed95a9fcae5760e5f346090eb6ef75eb40e547
emir-marukic <marukicemir33@hotmail.com>
2023-04-26 18:13:27 +0200

    while loop vezba

---
+++ Java-Script/Vezba/loop vezbe/while/app.js
@@ -0,0 +1,22 @@
+// Ispis prirodnih brojeve od 5 do 20 unazad.
+// let i = 20;
+// while (i >= 5) {
+//   console.log(i);
+//   i--;
+// }
+
+// Napisati program za ispis dvostruke vrijednosti prvih 5 prirodnih brojeva.
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i * 2);
+//   i++;
+// }
+
+// Napisati program za ispis kvadrata prirodnih brojeva od 1 do 5.
+
+// let i = 1;
+// while (i < 6) {
+//   console.log(i * i);
+//   i++;
+// }
