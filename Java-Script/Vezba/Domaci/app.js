c4a6c199d545cc28b7341c3ecfa65e292affd142
emir-marukic <marukicemir33@hotmail.com>
2023-04-28 14:25:34 +0200

    domaci

--- Java-Script/Vezba/Domaci/app.js
+++ Java-Script/Vezba/Domaci/app.js
@@ -8,32 +8,73 @@
 // Ispisuju se brojevi od manjeg ka vecem.
 
 // 1.
-let i = 1;
-while (i < 11) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-  i++;
-}
+// let i = 1;
+// while (i < 11) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
 //-------------------------------
 
 // 2.
 
-let n = +prompt("Unesite korektan broj ");
-let k = +prompt("Unesite korektan broj ");
+// let n = +prompt("Unesite korektan broj ");
+// let k = +prompt("Unesite korektan broj ");
 
-if (isNaN(n) || isNaN(k)) {
-  console.log("Niste uneli korektan broj");
-} else if (n <= k) {
-  while (n <= k) {
-    console.log(n);
-    n++;
-  }
-} else if (k < n) {
-  while (k <= n) {
-    console.log(k);
-    k++;
-  }
-} else {
-  console.log("Uneli ste iste brojeve");
-}
+// if (isNaN(n) || isNaN(k)) {
+//   console.log("Niste uneli korektan broj");
+// } else if (n <= k) {
+//   while (n <= k) {
+//     console.log(n);
+//     n++;
+//   }
+// } else if (k < n) {
+//   while (k <= n) {
+//     console.log(k);
+//     k++;
+//   }
+// } else {
+//   console.log("Uneli ste iste brojeve");
+// }
+
+//-------------------------------
+// Domaci zadaci:
+
+// 1.
+// Korisnik unosi broj iz intervala [12,16).
+// Na osnovu unetog broja, iteracija se vrsi od njega do 1(ukljucujuci).
+// Ispisati svaki broj iz iteracije, njegov kvadrat i vrednost broja umanjenu za 25.
+
+// 2.  Задатак: Фудбалски терен
+//     Фудбалски терен димензија d×s треба оградити правоугаоном оградом тако да је растојање страница ограде
+//     од линије терена r. Напиши програм који одређује дужину ограде.
+//     Улаз: У три реда стандардног улаза налазе се три цела броја:
+//     • d - дужина терена у метрима (90 ≤ d ≤ 120)
+//     • s - ширина терена у метрима (45 ≤ s ≤ 90)
+//     • r - растојање ограде од терена у метрима (2 ≤ r ≤ 10)
+//-------------------------------
+
+// let duzina = +prompt("Unesite duzinu ");
+// let sirina = +prompt("Unesite sirinu ");
+// let rastojanje = +prompt("Unesite rastojanje ");
+// const duzinaOgrade = 2 * duzina + 2 * sirina + 8 * rastojanje;
+
+// if (isNaN(duzina) || isNaN(sirina) || isNaN(rastojanje)) {
+//   console.log("Niste uneli dimenzije");
+// } else if (duzina <= 89 || duzina >= 120) {
+//   console.log("Niste uneli korektne dimenzije za duzinu");
+// } else if (sirina <= 44 || sirina >= 90) {
+//   console.log("Niste uneli korektne dimenzije za sirinu");
+// } else if (rastojanje <= 2 || rastojanje >= 10) {
+//   console.log("Niste uneli korektne dimenzije za rastojanje");
+// } else {
+//   console.log("Treba nam" + duzinaOgrade + "metara za ogradjivanje terena.");
+// }
+//-------------------------------
+// let i = +prompt("Unesite broj u intervalu 12 - 16");
+// if (i >= 12 && i <= 16) {
+//   for (i; i >= 1; i--) {
+//     console.log(i * i - 25);
+//   }
+// }
