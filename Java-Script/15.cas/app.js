ac0d896b3adf64d776d5122ad503b73c8f63fc64
emir-marukic <marukicemir33@hotmail.com>
2023-05-10 21:19:00 +0200

    string metode

---
+++ Java-Script/15.cas/app.js
@@ -0,0 +1,44 @@
+// const isSunny = true;
+
+// if (isSunny) {
+//   console.log("Napolju je suncano.");
+// } else {
+//   console.log("Napolju nije sunacno");
+// }
+
+// Postoji 8 falsy vrednosti:
+// 1. false
+// 2. ""
+// 3. 0
+// 4. -0
+// 5. 0n
+// 6. NaN
+// 7. undefined
+// 8. null
+
+// ifTrue = () => {
+//   const word = prompt("Unesite poruku: ");
+
+//   if (word) {
+//     return "TACNO";
+//   } else {
+//     return "NIJE TACNO";
+//   }
+// };
+// console.log(ifTrue());
+
+// ifTruthyNum = () => {
+//   const num = +prompt("Unesite neki broj: ");
+//   if (num) {
+//     return "TACNO";
+//   } else {
+//     return "NIJE TACNO";
+//   }
+// };
+// console.log(ifTruthyNum());
+
+//Trazi se unos  od strane korisnika sve dok ne unese bilo sta:
+// let recenica = "";
+// while (!recenica) {
+//   recenica = prompt("Unesite neku recenicu");
+// }
---
+++ Java-Script/15.cas/app2.js
@@ -0,0 +1,18 @@
+// STRINGOVI //
+// Stringovi su immutable vrednosti.
+// String su primitivnog(osnovnog) tipa podataka.
+
+// Za dobijanje duzine stringa koristimo length metodu:
+let a = "string";
+duzina = a.length;
+
+//String mozemo zapisati na nekoliko nacina:
+
+// 1. Koristeci duple navodnike: - "Danas je bilo promenljivo vreme"
+// 2. Koristeci obicne navodnike: -'Danas je bilo promenljivo vreme'
+// 3. Koristeci template literals - (Back-Ticks sintaksa) - `Danas je promenljivo vreme`
+
+// Dupli navodnici:
+// "Danas je bilo 'promenljivo' vreme"
+
+// "Danas je bilo \"promenljivo\" vreme"
