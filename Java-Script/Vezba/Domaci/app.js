b563c2af5261835ee643caf7b044f0e10eb10018
emir-marukic <marukicemir33@hotmail.com>
2023-05-13 21:24:01 +0200

    domaci

--- Java-Script/Vezba/Domaci/app.js
+++ Java-Script/Vezba/Domaci/app.js
@@ -209,25 +209,83 @@
 // 3. pola recenice ispisano velikim slovima, pola recenice ispisano malim slovima
 // 4. Ako se u recenici nadje rec 'skola' neka bude zamenjena recju fakultet.
 // Zadatak: Zamena reči
-// Napiši funkciju koja prima dva argumenta: originalni string i reč koju treba zameniti. Funkcija treba da zameni sve instance te reči u originalnom stringu sa znakom "#" i vrati rezultujući string. Na primer, za ulazne vrednosti "Danas je lep dan." i "lep", funkcija treba da vrati "Danas je # dan.".
+// Napiši funkciju koja prima dva argumenta: originalni string i reč koju treba zameniti. Funkcija treba da zameni sve instance te reči u originalnom
+// stringu sa znakom "#" i vrati rezultujući string. Na primer, za ulazne vrednosti "Danas je lep dan." i "lep", funkcija treba da vrati "Danas je # dan.".
 // Zadatak: Brojanje karaktera
 // Napiši funkciju koja prima jedan string kao argument i vraća broj karaktera u tom stringu. Ignoriši prazne prostore. Na primer, za ulazni string "Hello, World!", funkcija treba da vrati broj 12.
 
-myFunc = (recenica) => {
-  let malaSlova = recenica.toLowerCase();
-  let velikaSlova = recenica.toUpperCase();
-  let zamenaReci = recenica.replace(/skola/g, "fakultet");
-  let polaVelikaPolaMala = "";
-  for (let i = 0; i < recenica.length; i++) {
-    if (i < recenica.length / 2) {
-      return (polaVelikaPolaMala += recenica[i].toUpperCase());
-    } else {
-      return (polaVelikaPolaMala += recenica[i].toLowerCase());
-    }
-  }
-  let novaRecenica =
-    velikaSlova + " " + malaSlova + " " + polaVelikaPolaMala + " " + zamenaReci;
-  return novaRecenica;
-};
+// myFunc = (recenica) => {
+//   let malaSlova = recenica.toLowerCase();
+//   let velikaSlova = recenica.toUpperCase();
+//   let zamenaReci = recenica.replace(/skola/g, "fakultet");
+//   let polaVelikaPolaMala = "";
+//   for (let i = 0; i < recenica.length; i++) {
+//     if (i < recenica.length / 2) {
+//       return (polaVelikaPolaMala += recenica[i].toUpperCase());
+//     } else {
+//       return (polaVelikaPolaMala += recenica[i].toLowerCase());
+//     }
+//   }
+//   let novaRecenica =
+//     velikaSlova + " " + malaSlova + " " + polaVelikaPolaMala + " " + zamenaReci;
+//   return novaRecenica;
+// };
+// console.log(myFunc("Danas necu ici u skoli jer mi je dosadno"));
+//-------------------------------
+// Domaci zadatak.
+// Napraviti funkciju koja trazi od korisnika da unese neku recenicu, zatim funkcija vraca, datu recenicu u vise oblika (spojenu u jedan string).
+// 1. recenicu ispisanu velikim slovima,
+// 2. recenicu ispisanu malim slovima,
+// 3. pola recenice ispisano velikim slovima, pola recenice ispisano malim slovima
+// 4. Ako se u recenici nadje rec 'skola' neka bude zamenjena recju fakultet.
 
-console.log(myFunc("na vrh brda vrba mrda skola"));
+// myFunc = (recenica) => {
+//   let velikoSlovo = recenica.toUpperCase().replace(/SKOLA/g, "FAKULTET");
+//   let maloSlovo = recenica.toLowerCase().replace(/skola/g, "fakultet");
+//   let polaVelikaPolaMala = "";
+
+//   for (let i = 0; i < recenica.length; i++) {
+//     if (i < recenica.length / 2) {
+//       polaVelikaPolaMala += recenica[i]
+//         .toUpperCase()
+//         .replace(/SKOLA/gi, "FAKULTET");
+//     } // else if (recenica[i] === "skola") {
+//     //   polaVelikaPolaMala += recenica[i].replace(/skola/gi, "fakultet");
+//     //  } else if (recenica[i] === "SKOLA") {
+//     //   polaVelikaPolaMala += recenica[i].replace(/SKOLA/gi, "FAKULTET");
+//     //}
+//     else {
+//       polaVelikaPolaMala += recenica[i]
+//         .toLowerCase()
+//         .replace(/skola/gi, "fakultet");
+//     }
+//   }
+//   let zavrsnaRecenica = `Ovo su nase nove recenice:\n Velika slova - ${velikoSlovo}\n Mala Slova - ${maloSlovo}\n Mesana Slova - ${polaVelikaPolaMala}`;
+
+//   return zavrsnaRecenica;
+// };
+// console.log(myFunc("Danas je lose vreme da bi skola radio"));
+//------------------------------------------
+// Napiši funkciju koja prima dva argumenta: originalni string i reč koju treba zameniti. Funkcija treba da zameni sve instance te reči u originalnom
+// stringu sa znakom "#" i vrati rezultujući string. Na primer, za ulazne vrednosti "Danas je lep dan." i "lep", funkcija treba da vrati "Danas je # dan.".
+
+// myFunc = (recenica, rec) => {
+//   if (rec) {
+//     return recenica.replace(rec, "#");
+//   }
+// };
+// console.log(myFunc("Danas je lep dan", "dan"));
+//------------------------------------------
+// Zadatak: Brojanje karaktera
+// Napiši funkciju koja prima jedan string kao argument i vraća broj karaktera u tom stringu. Ignoriši prazne prostore. Na primer, za ulazni string "Hello, World!", funkcija treba da vrati broj 12.
+
+// myFunc = (recenica) => {
+//   let brojac = 0;
+//   for (let i = 0; i < recenica.length; i++) {
+//     if (recenica[i] !== " ") {
+//       brojac++;
+//     }
+//   }
+//   return brojac;
+// };
+// console.log(myFunc("la la"));
