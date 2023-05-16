3446e63aa48dd694c2e5906b72b8b809a2f40743
emir-marukic <marukicemir33@hotmail.com>
2023-05-16 15:44:43 +0200

    test

--- Java-Script/Vezba/Domaci/app.js
+++ Java-Script/Vezba/Domaci/app.js
@@ -318,12 +318,22 @@
 // };
 // console.log(second("danas je lep lep", "lep"));
 
-third = (recenica) => {
-  let brojac = 0;
-  for (let i = 0; i < recenica.length; i++) {
-    if (recenica[i] !== " ") {
-      brojac++;
-    }
-  }
-};
-console.log(third("la la"));
+// third = (recenica) => {
+//   let brojac = 0;
+//   for (let i = 0; i < recenica.length; i++) {
+//     if (recenica[i] !== " ") {
+//       brojac++;
+//     }
+//   }
+// };
+// console.log(third("la la"));
+// Domaci:
+// Zadatak: Spajanje stringova
+// Napiši funkciju koja prima dva stringa kao argumenta i vraća njihovu konkatenaciju.
+// Na primer, za ulazne vrednosti "Hello" i "World!", funkcija treba da vrati "HelloWorld!".
+
+// Zadatak: Palindrom
+// Napiši funkciju koja prima jedan string kao argument i proverava da li je taj string palindrom,
+// tj. da li se čita isto i s leva na desno i s desna na levo.
+// Funkcija treba da vrati true ako je string palindrom, inače false.
+// Na primer, za ulazni string "radar" funkcija treba da vrati true, dok za ulazni string "hello" funkcija treba da vrati false.
