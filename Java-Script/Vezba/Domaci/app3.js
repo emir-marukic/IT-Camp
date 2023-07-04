e1df2b550ae02a449cac2335d4a0aa0a7dde82f5
emir-marukic <marukicemir33@hotmail.com>
2023-07-04 18:13:20 +0200

    domaci

---
+++ Java-Script/Vezba/Domaci/app3.js
@@ -0,0 +1,37 @@
+// Domaci:
+// 1. Pomocu map() metode napraviti novi niz stringova ciji ce elementi da budu
+// ispisani velikim slovima.
+// const niz = ["emir", "nedim", "selver", "ajsa", "asija", "anastasija"];
+
+// const velikaSlova = niz.map((element) => element.toUpperCase());
+// console.log(velikaSlova);
+//-----------------
+// 2. Napraviti novi niz koji sadrzi element na kubni stepen postojeceg niza:
+
+// const brojevi = [1, 2, 3, 4, 5];
+
+// const kubniStepen = brojevi.map((num) => num ** 3);
+
+// console.log(kubniStepen);
+//-----------------
+// 3. Napraviti novi niz koji sadrzi kvadratne korene elemenata postojeceg niza.
+
+// const brojevi = [1, 2, 3, 4, 5];
+
+// const kvadratniKoren = brojevi.map((num) => Math.sqrt(num).toFixed(2));
+// console.log(kvadratniKoren);
+
+// 4.
+// const brojevi = [-5, 4, 3, 10, 14, -5];
+// Napraviti novi niz koji ce sve negativne brojeve pomnoziti sa (-1), a pozitivne kvadrirati.
+
+// const brojevi = [-3, 2, -5, 4, 0];
+
+// const razlika = brojevi.map((num) => {
+//   if (num > 0) {
+//     return num ** 2;
+//   } else {
+//     return num * -1;
+//   }
+// });
+// console.log(razlika);
