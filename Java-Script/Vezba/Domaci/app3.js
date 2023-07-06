c60d27ca6ba0c4dd6cde5c5bce343306fea205d8
emir-marukic <marukicemir33@hotmail.com>
2023-07-06 17:31:44 +0200

    domaci

--- Java-Script/Vezba/Domaci/app3.js
+++ Java-Script/Vezba/Domaci/app3.js
@@ -20,6 +20,7 @@
 
 // const kvadratniKoren = brojevi.map((num) => Math.sqrt(num).toFixed(2));
 // console.log(kvadratniKoren);
+//-----------------
 
 // 4.
 // const brojevi = [-5, 4, 3, 10, 14, -5];
@@ -35,3 +36,90 @@
 //   }
 // });
 // console.log(razlika);
+//-----------------
+
+// 1. Napraviti novi niz koji filtrira postojeci i vraca novi sa onim elementima cija je duzina <= 6.
+
+// const niz = ["emir", "nedim", "selver", "ajsa", "asija", "anastasija"];
+
+// const lessThen6 = niz.filter((name) => name.length <= 6);
+
+// console.log(lessThen6);
+//-----------------
+
+//  2. Napraviti novi niz koji filtrira postojeci i vraca novi
+//  sa onim elementima ciji je tip "boolean".
+
+// const niz = [
+//   "emir",
+//   "nedim",
+//   "selver",
+//   "ajsa",
+//   "asija",
+//   "anastasija",
+//   true,
+//   false,
+// ];
+
+// const isBoolean = niz.filter(
+//   (element) => element === true || element === false
+// );
+
+// console.log(isBoolean);
+
+//-----------------
+
+// 3. Napraviti funkciju koja pravi niz koji ce iz postojeceg napraviti novi uz ispunjenje uslova:
+//    value > 2 => value * 7
+//    value > 2 and value <8 => (value*4)/2
+//    Ostale vrednosti da ne uzima u obzir
+//    I na kraju treba novi niz vratiti sa godinama <10.
+
+// const niz = [10, 20, 3, 4, 6, 2, 12];
+
+// const transformedArr = (arr) => {
+//   const newArr = arr
+//     .filter((element) => element > 2)
+//     .map((value) => {
+//       if (value < 8) {
+//         return (value * 4) / 2;
+//       } else {
+//         return value * 7;
+//       }
+//     })
+//     .filter((value) => value < 10);
+//   return newArr;
+// };
+// console.log(transformedArr(niz));
+//-----------------
+
+// 4. Write a function that converts an array of values from miles to kilometres using the map method.
+// In the end, add the kilometres up in a new variable called "totalDistanceInKilometers" and
+// return this variable.
+
+// const milje = [10, 20, 3, 4, 6, 2, 12];
+
+// const milesToKm = (arr) => {
+//   const totalDistanceInKilometers = arr.map((value) =>
+//     (value * 1.60934).toFixed(2)
+//   );
+//   return totalDistanceInKilometers;
+// };
+// console.log(milesToKm(milje));
+//-----------------
+
+// 5. There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays.
+// Go to the editor
+
+// Expected Output :
+// [4, 5, 8, 10, 12, 13]
+
+// const array1 = [1, 0, 2, 3, 4];
+// const array2 = [3, 5, 6, 7, 8, 13];
+
+// const sum = (arr1, arr2) => {
+//   const result = arr1.map((value, index) => value + arr2[index] || 0);
+//   return result;
+// };
+
+// console.log(sum(array1, array2));
