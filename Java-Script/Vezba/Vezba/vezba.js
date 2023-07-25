149029d4b3c77ddc1861732ec64f0a722182e7aa
emir-marukic <marukicemir33@hotmail.com>
2023-07-25 17:52:37 +0200

    vezba

--- Java-Script/Vezba/Vezba/vezba.js
+++ Java-Script/Vezba/Vezba/vezba.js
@@ -1,6 +1,6 @@
 // const dragonAndAmmo = (dragon, bullet) => {
 //   for (let i = 0; i < dragon; i++) {
-//     bullet = bullet - 2;
+//     bullet -= 2;
 //     console.log(bullet);
 //     if (bullet < 0) {
 //       return false;
