5def92fbad500042bce4c37eaf442c4dc74dde78
emir-marukic <marukicemir33@hotmail.com>
2023-07-30 18:27:06 +0200

    domaci

---
+++ Java-Script/Vezba/Domaci/1.domaci/app.js
@@ -0,0 +1,11 @@
+// 1.  Iz 2 poslata niza vratiti 3. niz koji ce sadrzati one elemente iz oba niza koji nisu sadrzani u onom drugom nizu.
+// const niz1 = [1, 3, 4, 5, 6, 7, 9];
+// const niz2 = [2, 4, 5, 6, 8, 10];
+
+// const different = (arr1, arr2) => {
+//   const newArr1 = arr1.filter((el) => !arr2.includes(el));
+//   const newArr2 = arr1.concat(arr2.filter((el) => !arr1.includes(el)));
+//   newArr2.sort((a, b) => a - b);
+//   return newArr2;
+// };
+// console.log(different(niz1, niz2));
