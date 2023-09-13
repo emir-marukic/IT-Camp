eec35bf0f04a11073922641a1f57093578d9282d
emir-marukic <marukicemir33@hotmail.com>
2023-09-13 20:59:56 +0200

    prvi cas

---
+++ React/1.cas/index.js
@@ -0,0 +1,19 @@
+// primitives
+// complex
+// objects
+// arrays methods
+// callbacks
+// promises
+// async await
+// classes
+// DOM
+// DOM manipulation
+// events
+
+const obj = { size: 12, width: 22 };
+
+const getParam = (obj, prop) => {
+  return obj[property];
+};
+
+console.log(getParam(obj));
