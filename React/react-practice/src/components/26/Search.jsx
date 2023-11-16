1b1cd336cc88130c92995429a3297ebc2a099039
emir-marukic <marukicemir33@hotmail.com>
2023-11-16 20:53:57 +0100

    prevPage

--- React/react-practice/src/components/26/Search.jsx
+++ React/react-practice/src/components/26/Search.jsx
@@ -3,64 +3,127 @@ import "./Search.css";
 
 const Search = () => {
   const [data, setData] = useState({});
-  const [search, setSearch] = useState("");
+  const [searchQ, setSearchQ] = useState("");
 
   const getQuote = async () => {
     try {
-      let apiURL = "https://api.quotable.io/search/quotes?";
+      let apiURL = "https://api.quotable.io/search/quotes";
 
-      apiURL += "query=" + search;
+      apiURL += "?query=" + searchQ;
 
       const response = await fetch(apiURL);
       const responseData = await response.json();
+
       setData(responseData);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const nextPage = async () => {
+    try {
+      if (data.page === data.totalPages) {
+        return;
+      }
+
+      let apiURL = "https://api.quotable.io/search/quotes";
+
+      const toNextPage = data?.page + 1;
+
+      apiURL += "?query=" + searchQ;
+      apiURL += "&page=" + toNextPage;
+
+      const response = await fetch(apiURL);
+      const responseData = await response.json();
+
+      setData(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const prevPage = async () => {
+    try {
+      if (data.page < 0) {
+        return;
+      }
+      let apiURL = "https://api.quotable.io/search/quotes";
+
+      const toPrevPage = data?.page - 1;
+
+      apiURL += "?query=" + searchQ;
+      apiURL += "&page=" + toPrevPage;
+
+      const response = await fetch(apiURL);
+      const responseData = await response.json();
+
+      setData(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   console.log(data);
 
   return (
     <div>
       <input
+        value={searchQ}
         type="text"
-        name="search"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => setSearchQ(e.target.value)}
       />
-      <button onClick={getQuote}>Click</button>
+      <button onClick={getQuote}>Get quote</button>
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "20px",
-          marginTop: "20px",
-        }}
-      >
-        {data?.results?.map((quote, index) => {
+      <div>
+        {data && data.results?.length === 0 && <p>No quotes found</p>}
+        {data?.results?.map((quote) => {
           return (
-            <div key={index} className="box">
+            <div
+              key={quote._id}
+              style={{
+                border: "1px black solid",
+                padding: 10,
+                marginBottom: 10,
+                borderRadius: 4,
+              }}
+            >
               <p>{quote.author}</p>
               <p>{quote.content}</p>
             </div>
           );
         })}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          width: "200px",
-        }}
-      >
-        <button>{"<"}</button>
-        <button style={{ background: data?.page === 1 ? "blue" : "white" }}>
-          1
-        </button>
-        <button>2</button>
-        <button>3</button>
-        <button>{">"}</button>
+        <div style={{ display: "flex" }}>
+          <button onClick={prevPage}>{"<"}</button>
+          {data?.totalPages >= 1 && (
+            <button
+              style={{
+                backgroundColor: data?.page === 1 ? "white" : "buttonface",
+              }}
+            >
+              1
+            </button>
+          )}
+          {data?.totalPages >= 2 && data?.page < data?.totalPages && (
+            <button
+              style={{
+                backgroundColor: data?.page > 1 ? "white" : "buttonface",
+              }}
+            >
+              {data?.page > 1 ? data?.page : 2}
+            </button>
+          )}
+          {data?.totalPages >= 3 && (
+            <button
+              style={{
+                backgroundColor:
+                  data?.page === data?.totalPages ? "white" : "buttonface",
+              }}
+            >
+              {data.totalPages}
+            </button>
+          )}
+          <button onClick={nextPage}>{">"}</button>
+        </div>
       </div>
     </div>
   );
