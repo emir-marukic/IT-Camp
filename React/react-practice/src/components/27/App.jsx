7011ca3993828fac38dc7bc9c7ba78a967163163
emir-marukic <marukicemir33@hotmail.com>
2023-11-22 18:30:12 +0100

    axios

---
+++ React/react-practice/src/components/27/App.jsx
@@ -0,0 +1,11 @@
+import Search2 from "./Search";
+
+function Class27() {
+  return (
+    <div>
+      <Search2 />
+    </div>
+  );
+}
+
+export default Class27;
--- React/react-practice/src/main.jsx
+++ React/react-practice/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Class26 from "./components/26/App";
+
+import Class27 from "./components/27/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Class26 />
+    <Class27 />
   </React.StrictMode>
 );
